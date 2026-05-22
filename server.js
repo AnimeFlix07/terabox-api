8b49f08bce27be26352956e72544fce3cb46f069
AnimeFlix07 <abu.bakkar.blogger@gmail.com>
2026-05-22 08:02:10 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,33 +2,41 @@ import express from "express";
 import TeraBox from "./api.js";
 
 const app = express();
+
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-    res.send("TeraBox API Running");
+    res.json({
+        status: "TeraBox API Running"
+    });
 });
 
 app.get("/info", async (req, res) => {
+
     try {
+
         const url = req.query.url;
 
         if (!url) {
             return res.status(400).json({
-                error: "Provide terabox url"
+                error: "Missing terabox url"
             });
         }
 
-        const tb = new TeraBox();
+        const terabox = new TeraBox();
 
-        const data = await tb.get(url);
+        const data = await terabox.get(url);
 
         res.json(data);
 
-    } catch (e) {
+    } catch (err) {
+
         res.status(500).json({
-            error: e.message
+            error: err.message
         });
+
     }
+
 });
 
 app.listen(PORT, () => {
